649a79cfd789f795dd2b458c4a03a81aced60698
Bubbies <66354328+bub-bl@users.noreply.github.com>
2026-01-04 15:52:59 +0100

    Refactor icon rendering. Remove code complexity

--- LucideBlazor/IconBase.cs
+++ LucideBlazor/IconBase.cs
@@ -1,10 +1,10 @@
-﻿using LucideBlazor.Extensions;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using LucideBlazor.Extensions;
 
 namespace LucideBlazor;
 
-public abstract class IconBase : IComponent
+public abstract class IconBase : ComponentBase
 {
     [Parameter] public string? ClassName { get; set; }
     [Parameter] public int Size { get; set; } = 24;
@@ -15,98 +15,36 @@ public abstract class IconBase : IComponent
     [Parameter] public StrokeLineJoin StrokeLineJoin { get; set; } = StrokeLineJoin.Round;
 
     [Parameter(CaptureUnmatchedValues = true)]
-    public Dictionary<string, object>? Props { get; set; }
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
 
-    private RenderHandle _handle;
+    protected abstract string SvgContent { get; }
 
-    public void Attach(RenderHandle renderHandle)
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
-        _handle = renderHandle;
-    }
-
-    protected void RenderIcon(RenderTreeBuilder builder, string svgContent)
-    {
-        var counter = 0;
-
         builder.OpenElement(0, "svg");
-        builder.AddAttribute(counter++, "class", ClassName);
 
-        AddIfNotInProps("xmlns", "http://www.w3.org/2000/svg");
-        AddIfNotInProps("width", Size);
-        AddIfNotInProps("height", Size);
-        AddIfNotInProps("viewBox", "0 0 24 24");
-        AddIfNotInProps("fill", Fill);
-        AddIfNotInProps("stroke", Stroke);
-        AddIfNotInProps("stroke-width", StrokeWidth);
-        AddIfNotInProps("stroke-linecap", StrokeLineCapExtensions.ToString(StrokeLineCap));
-        AddIfNotInProps("stroke-linejoin", StrokeLineJoinExtensions.ToString(StrokeLineJoin));
+        var attributes = new Dictionary<string, object>
+        {
+            { "xmlns", "http://www.w3.org/2000/svg" },
+            { "width", Size },
+            { "height", Size },
+            { "viewBox", "0 0 24 24" },
+            { "fill", Fill },
+            { "stroke", Stroke },
+            { "stroke-width", StrokeWidth },
+            { "stroke-linecap", StrokeLineCapExtensions.ToString(StrokeLineCap) },
+            { "stroke-linejoin", StrokeLineJoinExtensions.ToString(StrokeLineJoin) }
+        };
 
-        if (Props is not null)
-            builder.AddMultipleAttributes(counter++, Props);
+        if (ClassName is not null)
+            attributes["class"] = ClassName;
 
-        builder.AddMarkupContent(counter, svgContent);
+        if (AdditionalAttributes is not null)
+            foreach (var attr in AdditionalAttributes)
+                attributes[attr.Key] = attr.Value;
+
+        builder.AddMultipleAttributes(1, attributes);
+        builder.AddMarkupContent(2, SvgContent);
         builder.CloseElement();
-
-        return;
-
-        // Helper to add attribute if not present in Props
-        void AddIfNotInProps(string name, object value)
-        {
-            if (Props is null || !Props.ContainsKey(name))
-                builder.AddAttribute(counter++, name, value);
-        }
-    }
-
-    protected abstract void HandleRender(RenderTreeBuilder builder);
-
-    public Task SetParametersAsync(ParameterView parameters)
-    {
-        foreach (var parameter in parameters)
-        {
-            switch (parameter.Name)
-            {
-                case nameof(ClassName):
-                    ClassName = (string?)parameter.Value;
-                    break;
-                case nameof(Size):
-                    Size = (int)parameter.Value;
-                    break;
-                case nameof(Fill):
-                    Fill = (string)parameter.Value;
-                    break;
-                case nameof(Stroke):
-                    Stroke = (string)parameter.Value;
-                    break;
-                case nameof(StrokeWidth):
-                    StrokeWidth = parameter.Value is int i ? i : (double)parameter.Value;
-                    break;
-                case nameof(StrokeLineCap):
-                    StrokeLineCap = parameter.Value switch
-                    {
-                        int capInt => (StrokeLineCap)capInt,
-                        string str => StrokeLineCapExtensions.FromString(str),
-                        StrokeLineCap capEnum => capEnum,
-                        _ => StrokeLineCap
-                    };
-                    break;
-                case nameof(StrokeLineJoin):
-                    StrokeLineJoin = parameter.Value switch
-                    {
-                        int joinInt => (StrokeLineJoin)joinInt,
-                        string str => StrokeLineJoinExtensions.FromString(str),
-                        StrokeLineJoin joinEnum => joinEnum,
-                        _ => StrokeLineJoin
-                    };
-                    break;
-                default:
-                    Props ??= new Dictionary<string, object>();
-                    Props[parameter.Name] = parameter.Value;
-
-                    break;
-            }
-        }
-
-        _handle.Render(HandleRender);
-        return Task.CompletedTask;
     }
 }
--- LucideBlazor/LucideIcon.cs
+++ LucideBlazor/LucideIcon.cs
@@ -1,45 +1,11 @@
-using LucideBlazor.Extensions;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
 
 namespace LucideBlazor;
 
-public class LucideIcon : ComponentBase
+public class LucideIcon : IconBase
 {
-    [Parameter] public string Name { get; set; } = string.Empty;
-    [Parameter] public string? ClassName { get; set; }
-    [Parameter] public int Size { get; set; } = 24;
-    [Parameter] public string Fill { get; set; } = "none";
-    [Parameter] public string Stroke { get; set; } = "white";
-    [Parameter] public double StrokeWidth { get; set; } = 2;
-    [Parameter] public StrokeLineCap StrokeLineCap { get; set; } = StrokeLineCap.Round;
-    [Parameter] public StrokeLineJoin StrokeLineJoin { get; set; } = StrokeLineJoin.Round;
+    [Parameter]
+    public string Name { get; set; } = string.Empty;
 
-    [Parameter(CaptureUnmatchedValues = true)]
-    public Dictionary<string, object>? Props { get; set; }
-
-    protected override void BuildRenderTree(RenderTreeBuilder builder)
-    {
-        if (string.IsNullOrEmpty(Name)) return;
-
-        var iconType = IconMap.GetIconType(Name);
-        if (iconType is null) return;
-
-        builder.OpenComponent(0, iconType);
-
-        if (ClassName is not null)
-            builder.AddAttribute(1, nameof(IconBase.ClassName), ClassName);
-
-        builder.AddAttribute(2, nameof(IconBase.Size), Size);
-        builder.AddAttribute(3, nameof(IconBase.Fill), Fill);
-        builder.AddAttribute(4, nameof(IconBase.Stroke), Stroke);
-        builder.AddAttribute(5, nameof(IconBase.StrokeWidth), StrokeWidth);
-        builder.AddAttribute(6, nameof(IconBase.StrokeLineCap), StrokeLineCapExtensions.ToString(StrokeLineCap));
-        builder.AddAttribute(7, nameof(IconBase.StrokeLineJoin), StrokeLineJoinExtensions.ToString(StrokeLineJoin));
-
-        if (Props is not null)
-            builder.AddMultipleAttributes(8, Props);
-
-        builder.CloseComponent();
-    }
+    protected override string SvgContent => IconMap.GetIconSvg(Name) ?? string.Empty;
 }
